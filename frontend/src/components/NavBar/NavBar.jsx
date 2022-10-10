627937e9321f23333dcd2c761fbc1ce1316859f7
Eugene Kofman <eugenekofman@gmail.com>
2022-10-10 15:07:55 -0400

    work on nav complete for time being

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,32 +1,16 @@
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import skatelogo from '../../assets/logo/skatelogo.png'
 import SessionButton from './SessionButton';
 const NavBar = () => {
-    // const usePath = () => {
-    //     const location = useLocation();
-    //     return location.pathname;
-    // }
 
-    // const { login }  = useParams()
-    // console.log(useParams())
-    // console.log(login)
-    // debugger
-    // const buttonText = usePath() === '/login' ? "Sign Up" : "Log In"
-    // const buttonLink = usePath() === '/login' ? "/signup" : "/login"
-
-    // const buttonClassName = usePath() === '/login' ? 'signupBut' : 'loginBut' 
     
-
+    
     return ( 
 
         <div className='full-width'>
             <nav className='navbar'>
                 {/* <h1 className='cadencetitle'><NavLink  to='/'>Cadence</NavLink></h1> */}
                 <NavLink to='/'><img src={skatelogo}alt='cadencelogo' className='logo'></img></NavLink>
-
-                {/* <NavLink activeClassName="orange" to="/login">{buttonText}</NavLink> */}
-                {/* <NavLink to={buttonLink}>
-                <button className={buttonClassName}>{buttonText}</button></NavLink> */}
                 <SessionButton />
 
             </nav>
--- frontend/src/components/NavBar/SessionButton.jsx
+++ frontend/src/components/NavBar/SessionButton.jsx
@@ -1,14 +1,16 @@
 import { NavLink, useLocation } from 'react-router-dom';
 
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import * as sessionActions from '../../store/session'
 import { getSession } from "../../store/session";
 import { useState } from 'react';
 
 const SessionButton = () => {
+    const dispatch = useDispatch()
+
     
     const [errors, setErrors] = useState([])
-
+    
     const sessionUser = useSelector(getSession);
     
     
@@ -18,24 +20,48 @@ const SessionButton = () => {
     }
     // const logOutButton = 
     
-    const buttonText = usePath() === '/login' ? "Sign Up" : sessionUser ? "Log Out" : "Log In"
+    // const buttonText = usePath() === '/login' ? "Sign Up" : sessionUser ? "Log Out" : "Log In"
+    const buttonText = usePath() === '/login' ? "Sign Up" : "Log In"
+
     
     // const buttonText = usePath() === '/login' ? "Sign Up" : "Log In"
     const buttonLink = usePath() === '/login' ? "/signup" : "/login"
 
-    const buttonClassName = usePath() === '/login' ? 'signupBut' : 'loginBut' 
+    const buttonClassName = usePath() === '/login' ? 'signupBut' : sessionUser ? 'logoutBut' : 'loginBut' 
 
-    // handleClick = e => {
-    //     e.prevent default
-    // }
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        setErrors([]);
+        return dispatch (sessionActions.logout({ }))
+            .catch(async (res) => {
+                let data;
+                try {
+                    data = await res.clone().json();
+                } catch {
+                    data = await res.text();
+                }
+                if (data?.errors) setErrors([data]);
+                else setErrors(["you may have made a mistake"]);
+            });
+    }
+    const buttonReturned = sessionUser ? 
+        (
+            <form onSubmit={handleSubmit}>
+                <button>Log Out</button>
+            </form>
+        ) 
+        
+        : 
+        
+        ( 
     
-    return ( 
-        <>
             <NavLink to={buttonLink}>
                 <button className={buttonClassName}>{buttonText}</button>
             </NavLink>
-        </>
-     );
+
+        );
+    return buttonReturned
 }
  
 export default SessionButton;
