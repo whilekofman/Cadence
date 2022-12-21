b6f71e18222d1a7038cf77a3aa38b3af15e8c22d
Eugene Kofman <eugenekofman@gmail.com>
2022-12-21 10:18:15 -0500

    work on comment like toggle

--- frontend/src/components/Like/CommentLike.jsx
+++ frontend/src/components/Like/CommentLike.jsx
@@ -10,21 +10,25 @@ const CommentLike = ( { commentId } ) => {
     const likesFiltered = likes.filter(like => 
         like.likeableType === "Comment" && like.likeableId === commentId)
 
-    console.log(likesFiltered)
+    // console.log(likesFiltered)
+    let likeToggle = false
+    const heartClass = likeToggle ? "material-symbols-outlined not-liked" :  "comment-liked-color material-symbols-outlined liked"
 
-    const heartClass = ["material-symbols-outlined not-liked", "comment-liked-color material-symbols-outlined liked"]
+    // const heartClass = ["material-symbols-outlined not-liked", "comment-liked-color material-symbols-outlined liked"]
 
     const handleCommentLike = e => {
         e.preventDefault()
-        console.log(heartClass[0])
-        const swapHeartClass = heartClass.shift()
-        heartClass.push(swapHeartClass)
-        console.log(heartClass[0])
+        likeToggle = !likeToggle
+
+        // console.log(heartClass[0])
+        // const swapHeartClass = heartClass.shift()
+        // heartClass.push(swapHeartClass)
+        // console.log(heartClass[0])
 
     }
 
     return ( 
-        <div onClick={handleCommentLike} className={heartClass[0]}>favorite</div>
+        <div onClick={handleCommentLike} className={heartClass}>favorite</div>
      );
 }
  
