c28b1c626d53cc395ca1b213e12bb5fe81319596
Eugene Kofman <eugenekofman@gmail.com>
2022-12-21 20:50:31 -0500

    working comment likes

--- frontend/src/components/Like/CommentLike.jsx
+++ frontend/src/components/Like/CommentLike.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react"
-import { useSelector } from "react-redux"
-import { getLikes } from "../../store/likes"
+import { useDispatch, useSelector } from "react-redux"
+import { deleteLike, getLikes, newLike } from "../../store/likes"
 import { getSession } from "../../store/session"
 
 const CommentLike = ( { commentId } ) => {
     const [foo, setFoo] = useState(false)
     const currentUser = useSelector(getSession)
     const likes = useSelector(getLikes)
-    
+    const dispatch = useDispatch()
     const commentLikes = likes.filter(like => 
         like.likeableType === "Comment" && like.likeableId === commentId)
     
     const userLike = commentLikes.filter(like => like.likerId === currentUser.id)
     
     const commentLikeText = commentLikes.length ? `${commentLikes.length} likes` : ``
-    // console.log(userLike)
+    console.log(userLike)
     // console.log(commentLikes)
 
     // let likeToggle = false
@@ -25,7 +25,20 @@ const CommentLike = ( { commentId } ) => {
 
     const handleCommentLike = e => {
         e.preventDefault()
-        
+
+        if (userLike.length) {
+            dispatch(deleteLike(userLike[0].id))
+            console.log(userLike[0].id)
+        } else {
+            const like = {
+                likerId: currentUser.id,
+                likeableType: "Comment",
+                likeableId: commentId
+            
+            }
+            dispatch(newLike(like, like.likeableType, like.likeableId))
+        }
+
         setFoo(!foo)
         // console.log(foo, 'foo', heartClass, 'heartClass')
 
