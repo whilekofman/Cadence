20722fdd7513fc4a03241bf25eb1f924ba44f7d1
Eugene Kofman <eugenekofman@gmail.com>
2023-01-16 20:59:25 -0500

    extracted profile pictures and names to own components including links to user show pages also did work to followers page selector.  TODO update user and follow state on link click;

---
+++ frontend/src/components/AthleteName/AthleteName.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const AthleteName = ({ fname, lname, fullName, targetId }) => {
+    const displayName = `${fname} ${lname}` || fullName;
+    return (
+        <>
+            <Link className="athlete-name-link" to={`users/${targetId}`}>
+                {displayName}
+            </Link>
+        </>
+    );
+};
+
+export default AthleteName;
