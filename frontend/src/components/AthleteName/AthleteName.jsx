dfd67168bee21acc3533d205f2d537a55197c0a6
Eugene Kofman <eugenekofman@gmail.com>
2023-01-20 16:43:33 -0500

    fix but where name would show up in blue until clicked

--- frontend/src/components/AthleteName/AthleteName.jsx
+++ frontend/src/components/AthleteName/AthleteName.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 const AthleteName = ({ fname, lname, fullName, targetId, page }) => {
     const displayName = `${fname} ${lname}` || fullName;
+    const cssPage = page || "default"
     return (
         <>
             <Link className="athlete-name-link" to={`/users/${targetId}`}>
