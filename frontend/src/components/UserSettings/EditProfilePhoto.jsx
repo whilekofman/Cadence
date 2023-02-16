d7270b848bff39e2e3db2412cbc6d3cabced246a
Eugene Kofman <eugenekofman@gmail.com>
2023-02-16 18:42:20 -0500

    working photo crop for user pic update

--- frontend/src/components/UserSettings/EditProfilePhoto.jsx
+++ frontend/src/components/UserSettings/EditProfilePhoto.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getSession } from "../../store/session";
 import { updateUserProfilePicture } from "../../store/users";
-
+import Cropper from "react-easy-crop";
+import { getCroppedImg } from "../utils/canvas";
 const EditProfilePhoto = ({
     photoFile,
     setPhotoFile,
@@ -13,9 +14,18 @@ const EditProfilePhoto = ({
     const dispatch = useDispatch();
     const currentUser = useSelector(getSession);
 
-    const onSaveClick = () => {
+    const [crop, setCrop] = useState({ x: 0, y: 0 });
+    const [rotation, setRotation] = useState(0);
+    const [zoom, setZoom] = useState(1);
+    const [imageUrl, setImageUrl] = useState(null);
+    const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+    const [croppedPhotoFile, setCroppedPhotoFile] = useState(null);
+    const [croppedPhotoUrl, setCroppedPhotoUrl] = useState(null);
+
+    const onConfirmClick = () => {
         const formData = new FormData();
-        formData.append("user[profile_picture]", photoFile);
+        const fileToUpload = croppedPhotoFile ? croppedPhotoFile : photoFile;
+        formData.append("user[profile_picture]", fileToUpload);
         dispatch(updateUserProfilePicture(currentUser, formData));
         setPhotoFileUrl(null);
         setPhotoFile(null);
@@ -26,25 +36,80 @@ const EditProfilePhoto = ({
         setPhotoFile(null);
     };
 
+    const showCroppedImage = async () => {
+        try {
+            const { file, url } = await getCroppedImg(
+                photoFileUrl,
+                croppedAreaPixels
+            );
+            console.log("donee", { url, file });
+            setCroppedPhotoUrl(url);
+            setCroppedPhotoFile(file);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+        setCroppedAreaPixels(croppedAreaPixels);
+    }, []);
+
     return (
         <>
             <div className="edit-photo-top">
-                <h1 className="title">Confirm New Profile Photo</h1>
+                <h1 className="title">Edit Photo</h1>
+                <p className="edit-photo-instructions">
+                    Select the portion of the photo you'd like to use by using the mouse wheel or touch pad to zoom in or out, and you can drag the photo to the position you need using your mouse or touchpad. To see a preview of your cropped photo, click "Crop". To save your changes, click "Confirm". To cancel, click "Cancel".  
+                </p>
             </div>
             <div className="buttons-edit-photo">
                 <button
                     className="save-button-edit-photo"
-                    onClick={onSaveClick}
+                    onClick={showCroppedImage}
                 >
-                    Save
+                    Crop
+                </button>
+                <button
+                    className="save-button-edit-photo"
+                    onClick={onConfirmClick}
+                >
+                    Confirm
                 </button>
                 <div className="cancel-link" onClick={handleCancel}>
                     Cancel
                 </div>
             </div>
+            <div className="before-after-container">
+                <div className="before-container">
+                    <h3 className="crop-preview">Before Crop</h3>
+                    <div className="edit-image-container">
+                        <Cropper
+                            image={photoFileUrl}
+                            crop={crop}
+                            zoom={zoom}
+                            aspect={1 / 1}
+                            showGrid={true}
+                            onCropComplete={onCropComplete}
+                            onZoomChange={setZoom}
+                            style={{
+                                containerStyle: {
+                                    width: "100%",
+                                    height: "100%",
+                                    background: "#ffffff",
+                                },
+                            }}
+                            onCropChange={setCrop}
+                        />
+                    </div>
+                </div>
 
-            <div className="new-photo-container">
-                <img src={photoFileUrl} className="new-photo" />
+                {croppedPhotoUrl && (
+                    <div className="after-container">
+                    
+                        <h3 className="crop-preview-after">After Crop</h3>
+                        <img className="after" src={croppedPhotoUrl} />
+                    </div>
+                )}
             </div>
         </>
     );
